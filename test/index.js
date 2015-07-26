b9248244dfbae92936ca705ff1b7cdf12ef30755
Masaaki Morishita <ikasama3.0@gmail.com>
2015-07-27 02:42:47 +0900

    test code

--- test/index.js
+++ test/index.js
@@ -1,6 +1,26 @@
 var test = require('tape')
-var sort-border-values = require('..')
+var sort = require('..')
 
-test('description', function (t) {
+var value = '#ddd 1px solid'
+
+test('default', function (t) {
+    var actual = sort(value)
+    var expected = [
+        { width: '1px' },
+        { style: 'solid' },
+        { color: '#ddd' }
+    ]
+    t.same(actual, expected)
+    t.end()
+})
+
+test('default', function (t) {
+    var actual = sort(value, 'swc')
+    var expected = [
+        { style: 'solid' },
+        { width: '1px' },
+        { color: '#ddd' }
+    ]
+    t.same(actual, expected)
     t.end()
 })
