0a51ef439d316e73f1b4cac8de3c98007d729f6a
Masaaki Morishita <ikasama3.0@gmail.com>
2015-07-27 02:38:56 +0900

    first implementation

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
-module.exports = function () {
+var border = require('css-border-property')
+var parse = border.parse
+var stringify = border.stringify
+
+function getKey (obj) {
+    return Object.keys(obj)[0]
+}
+
+module.exports = function (values, fmt) {
+    var result = parse(values)
+    var width, style, color
+
+    result.forEach(function (decl, i) {
+        var prop = getKey(decl)
+        switch (prop) {
+            case 'width':
+                width = result[i]
+                break
+            case 'style':
+                style = result[i]
+                break
+            case 'color':
+                color = result[i]
+                break
+        }
+    })
+
+    if (!fmt) fmt = 'wsc'
+
+    switch (fmt) {
+        case 'wsc':
+            return [width, style, color]
+        case 'wcs':
+            return [width, color, style]
+        case 'swc':
+            return [style, width, color]
+        case 'scw':
+            return [style, color, width]
+        case 'cws':
+            return [color, width, style]
+        case 'csw':
+            return [color, style, width]
+    }
 }
